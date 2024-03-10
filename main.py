e6ad4b7f7e27145043b9545c37ba471581ec0607
Shaheer <64867718+ShaheerVD@users.noreply.github.com>
2024-03-10 12:45:58 +0200

    add task

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 import flet as ft
 
+class TodoApp(ft.UserControl):
+  def build(self):
+    self.new_task=ft.TextField(hint_text="What needs to be done?",expand=True)
+    self.tasks=ft.Column()
+
+    #app root control containing all other controls
+    return ft.Column(
+      width=600,
+      controls=[
+        ft.Row(
+          controls=[
+            self.new_task,
+            ft.FloatingActionButton(icon=ft.icons.ADD,on_click=self.add_clicked),
+          ],
+        ),
+        self.tasks,
+      ],
+    )
+
+  def add_clicked(self,e):
+    self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+    self.new_task.value=""
+    self.update()
+
+
 def main(page: ft.Page):
-  page.add(ft.Text(value="Hello,World!"))
+  page.title="ToDo App"
+  page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+  page.update()
+
+  #create app Instance
+  todo=TodoApp()
+
+  #add app root control to the page
+  page.add(todo)
+  
 
 ft.app(target=main)  
